20d63cf99a96d01ff3cd29a3d9a77d00883e8a7b
Jesse Hallett <jesse@sitr.us>
2014-10-09 16:03:53 -0700

    Gets list of mailing lists from mail archive

---
+++ Network/Mail/SocialMail/Notmuch.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- This is intended to emulate the api of Foreign.Notmuch; to be used as a shim
+-- until I get that library working.
+module Network.Mail.SocialMail.Notmuch where
+
+import Codec.MIME.Parse (parseHeaders)
+import Codec.MIME.Type (MIMEParam(..))
+import Data.CaseInsensitive (mk)
+import Data.Text (Text, pack)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import System.IO
+import System.Process (readProcess)
+
+data Database = Database { dLoc :: FilePath, dMode :: DatabaseMode  }
+
+data DatabaseMode =
+    DatabaseModeReadOnly |
+    DatabaseModeReadWrite
+    deriving Enum
+
+data Query = Query { qDb :: Database, qText :: String }
+
+data Thread = Thread { tId :: String }
+
+databaseOpen :: FilePath -> DatabaseMode -> IO Database
+databaseOpen f m = return (Database f m)
+
+queryCreate :: Database -> String -> IO Query
+queryCreate db q = return (Query db q)
+
+queryThreads :: Query -> IO [Thread]
+queryThreads q = do
+  out <- notmuch ["search", "--output=threads", qText q]
+  return $ map Thread (lines out)
+
+threadGetAuthors :: Thread -> IO [Text]
+threadGetAuthors = threadGetHeaderValues "From"
+
+threadGetRecipients :: Thread -> IO [Text]
+threadGetRecipients = threadGetHeaderValues "To"
+
+threadGetHeaderValues :: Text -> Thread -> IO [Text]
+threadGetHeaderValues h t = do
+  msgs <- fmap lines (notmuch ["search", "--output=files", tId t])
+  fmap concat (mapM (parseHeader h) msgs)
+
+notmuch :: [String] -> IO String
+notmuch args = readProcess "/usr/bin/notmuch" opts ""
+  where
+    opts = ["--config=/home/jesse/.notmuch-galois"] ++ args
+
+parseHeader :: Text -> FilePath -> IO [Text]
+parseHeader name f = withFile f ReadMode $ \handle -> do
+  hSetEncoding handle latin1
+  txt <- TIO.hGetContents handle
+  -- Hackish parsing here.  Where is the library that reads in an entire message
+  -- and returns headers?
+  let ls = takeWhile (\l -> T.length l > 0) (T.lines txt)
+  let ls' = filter (\l -> mk (T.take (T.length name) l) == mk name) ls
+  let (hs, _) = parseHeaders (T.unlines ls')
+  let tos = filter (\h -> mk (paramName h) == mk name) hs
+  return $ map paramValue tos
--- client.hs
+++ client.hs
@@ -1,12 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Data.DateTime (getCurrentTime)
+import Data.List (nub)
+import Data.Maybe (catMaybes)
 import Data.Text (Text, pack)
+import qualified Data.Text.IO as T
+import Data.Text.ICU (MatchOption(..), Regex, find, group, regex)
+-- import Foreign.Notmuch
+import Network.Mail.SocialMail.Notmuch
 import Network.Mail.Mime (Address(..), Mail(..), emptyMail, renderSendMailCustom)
 import Network.Mail.SocialMail.Compose
 
 main :: IO ()
 main = do
+  addrs <- getListAddrs
+  mapM_ T.putStrLn addrs
+
+compose :: IO ()
+compose = do
   putStrLn "from: "
   sender <- getLine
   putStrLn "to: "
@@ -18,3 +31,23 @@ main = do
   let mail = (emptyMail (toAddress sender)) { mailTo = [toAddress recip] }
   let mail' = addActivity note mail
   renderSendMailCustom "/usr/bin/msmtp" [recip] mail'
+
+toAddress :: String -> Address
+toAddress email = Address { addressName = Nothing, addressEmail = pack email }
+
+fromList :: Database -> IO Query
+fromList db = queryCreate db "from:lists.galois.com OR to lists.galois.com"
+
+getListAddrs :: IO [Text]
+getListAddrs = do
+  db <- databaseOpen "/home/jesse/mail/galois" DatabaseModeReadOnly
+  query <- fromList db
+  ts <- queryThreads query
+  ls <- fmap concat (mapM threadGetRecipients ts)
+  return $ nub (catMaybes (map parseListAddr ls))
+
+parseListAddr :: Text -> Maybe Text
+parseListAddr l = find listAddrExp l >>= group 0
+
+listAddrExp :: Regex
+listAddrExp = regex [CaseInsensitive] "[a-z]+@lists.galois.com"
