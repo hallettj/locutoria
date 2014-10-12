cc960556b6aedd24809f52b07755fb67f16dbc44
Jesse Hallett <jesse@sitr.us>
2014-10-11 17:19:01 -0700

    WIP: reading JSON data from notmuch

--- Network/Mail/SocialMail/Notmuch.hs
+++ Network/Mail/SocialMail/Notmuch.hs
@@ -4,9 +4,22 @@
 -- until I get that library working.
 module Network.Mail.SocialMail.Notmuch where
 
-import Codec.MIME.Parse (parseMIMEMessage)
-import Codec.MIME.Type (MIMEParam(..), MIMEValue(..))
+-- import Codec.MIME.Parse (parseMIMEMessage)
+-- import Codec.MIME.Type (MIMEParam(..), MIMEValue(..))
+import           Data.Aeson ( FromJSON(..)
+                            , ToJSON(..)
+                            , Value
+                            , fromJSON
+                            , object
+                            , (.=)
+                            , (.:)
+                            , (.:?)
+                            )
+import qualified Data.Aeson as Ae
+import Data.Aeson.TH
+import qualified Data.HashMap.Strict as HM
 import Data.CaseInsensitive (mk)
+import Data.Char (ord)
 import Data.Text (Text, pack)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
@@ -57,6 +70,38 @@ parseHeader :: Text -> FilePath -> IO [Text]
 parseHeader name f = withFile f ReadMode $ \handle -> do
   hSetEncoding handle latin1
   txt <- TIO.hGetContents handle
+  -- let txt' = pack (strip (T.unpack txt))
   let val = parseMIMEMessage txt
   let h = filter (\h -> mk (paramName h) == mk name) (mime_val_headers val)
+  mapM_ (TIO.putStrLn . paramValue) h
   return $ map paramValue h
+
+-- Strips control codes and extended characters.
+strip :: String -> String
+strip = filter (\x -> ord x > 32 && ord x < 126 || ord x `elem` [9, 10, 13])
+
+
+
+data Message = Message
+  { id :: Text
+  , headers :: [(Text, Text)]
+  , body :: [MessagePart]
+  }
+
+data MessagePart = MessagePart
+  { id :: Int
+  , contentType :: Text
+  }
+
+instance FromJSON Message where
+  parseJSON (Ae.Object o) =
+    Message <$> o .: "id"
+            <*> o .: "headers"
+            <*> o .: "body"
+  parseJSON _ = mzero
+
+instance FromJSON MessagePart where
+  parseJSON (Ae.Object o) =
+    MessagePart <$> o .: "id"
+                <*> o .: "content-type"
+  parseJSON _ = mzero
